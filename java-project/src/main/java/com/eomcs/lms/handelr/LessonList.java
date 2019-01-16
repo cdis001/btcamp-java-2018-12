6fcd66b5909edaf6e10b7dd27731360b1961f5cf
EuiJoo.Kim <shopingid@naver.com>
2019-01-16 19:43:19 +0900

    .

---
+++ java-project/src/main/java/com/eomcs/lms/handelr/LessonList.java
@@ -0,0 +1,5 @@
+package com.eomcs.lms.handelr;
+
+public class LessonList {
+
+}
--- java-project/src15/main/java/com/eomcs/lms/App.java
+++ java-project/src15/main/java/com/eomcs/lms/App.java
@@ -11,12 +11,15 @@ public class App {
   public static void main(String[] args) {
     
     BoardHandler bH = new BoardHandler(keyboard);
+    //생성자가 없으면, keyboard값을 받아오지 않더라도 App에서는 컴파일시 에러가 뜨지 않음! ->그러나, keyboard값이 없으면 실행시 에러가 뜸
+    //생성자가 있는 경우 keyboard값을 받아와야 컴파일시 에러가 뜨므로 keyboard값을 '강제로' 받아와야 함
     
     BoardHandler bH2 = new BoardHandler(keyboard);
     
     LessonHandler lH = new LessonHandler(keyboard);
     
     MemberHandler mH = new MemberHandler(keyboard);
+    //당장 하나만 쓰는 정보여도 나중에 재사용을 위하여 인스턴스 필드로 선언하는편이 좋음 
     
     
     while (true) {
@@ -46,7 +49,7 @@ public class App {
         System.out.println("실행할 수 없는 명령입니다.");
       }
 
-      System.out.println();
+      System.out.println(); // 결과 출력 후 빈 줄 출력
     }
 
     keyboard.close();
