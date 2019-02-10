552341ee69333c961fa416833ec350099bf3baca
cdis001 <shopingid@naver.com>
2019-02-11 01:37:01 +0900

    .

--- java-basic/src/main/java/ch23/c/CalculatorClient.java
+++ java-basic/src/main/java/ch23/c/CalculatorClient.java
@@ -1,35 +1,42 @@
 package ch23.c;
 
-import java.io.BufferedInputStream;
-import java.io.BufferedOutputStream;
-import java.io.DataOutputStream;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.PrintWriter;
+import java.io.DataInputStream;
+import java.io.PrintStream;
 import java.net.Socket;
 import java.util.Scanner;
 
 public class CalculatorClient {
   public static void main(String[] args) {
-    try (Socket socket = new Socket("localhost", 8888);
-        PrintWriter out = new PrintWriter(socket.getOutputStream());
-        Scanner in = new Scanner(socket.getInputStream())) {
+    try (Scanner keyboard = new Scanner(System.in);
+        Socket socket = new Socket("localhost", 8888);
+        PrintStream out = new PrintStream(socket.getOutputStream());
+        DataInputStream in = new DataInputStream(socket.getInputStream())) {
       System.out.println("계산기 서버에 오신 걸 환영합니다!");
-      while (true) {
-      System.out.println("계산식을 입력하세요!");
-      System.out.println("예) 23 + 7 ");
-      System.out.println("> ");
-      String a = keyboard.nextLine();
-      if (a.equalsIgnoreCase("quit")) {
-        break;
-      }
-      String request = in.nextLine();
-      out.printf("결과는 %d 입니다\n", request);
-      out.flush();
-      
+
       keyboard.nextLine();
       
+      while (true) {
+        int a = in.readInt();
+        String b = in.readUTF();
+        int c = in.readInt();
+        int result = 0;
+
+        switch (b) {
+          case "+": result = a + c; break;
+          case "-": result = a - c; break;
+          case "*": result = a * c; break;
+          case "/": result = a / c; break;
+        }
+
+        if(b.equalsIgnoreCase("quit")) {
+          break;
+        }
+        out.printf("결과는 %d입니다 \n", result);
       }
+      out.println("안녕히 가세요!");
+
+
+
     } catch(Exception e) {
       e.printStackTrace();
     }
--- java-basic/src/main/java/ch23/c/CalculatorServer.java
+++ java-basic/src/main/java/ch23/c/CalculatorServer.java
@@ -1,40 +1,48 @@
 package ch23.c;
 
-import java.io.PrintWriter;
+import java.io.DataOutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.Scanner;
 
 public class CalculatorServer {
-  
+
   public static void main(String[] args) {
     try(Scanner keyboard = new Scanner(System.in);
         ServerSocket serverSocket = new ServerSocket(8888);) {
       try(Socket socket = serverSocket.accept();
-          PrintWriter out = new PrintWriter(socket.getOutputStream());
+          DataOutputStream out = new DataOutputStream(socket.getOutputStream());
           Scanner in = new Scanner(socket.getInputStream())) {
 
         System.out.println("계산기 서버에 오신 걸 환영합니다!");
         while (true) {
-        System.out.println("계산식을 입력하세요!");
-        System.out.println("예) 23 + 7 ");
-        System.out.println("> ");
-        String a = keyboard.nextLine();
-        if (a.equalsIgnoreCase("quit")) {
-          break;
-        }
-        String request = in.nextLine();
-        out.printf("결과는 %d 입니다\n", request);
-        out.flush();
-        
-        keyboard.nextLine();
-        
+          System.out.println("계산식을 입력하세요!");
+          System.out.println("예) 23 + 7 ");
+          System.out.println("> ");
+          out.writeInt(Integer.parseInt(keyboard.nextLine()));
+
+          System.out.println("> ");
+          out.writeUTF(keyboard.nextLine());
+
+          System.out.println("> ");
+          out.writeInt(Integer.parseInt(keyboard.nextLine()));
+
+          String a = in.nextLine();
+          if (a.equalsIgnoreCase("quit")) {
+            break;
+          }
+
+          out.flush();
+          //        
+          keyboard.nextLine();
+
         }
       }
-      
+
     } catch(Exception e) {
       e.printStackTrace();
     }
     System.out.println("서버 종료!");
   }
 }
+
